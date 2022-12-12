a2cb9fb34eea522489187ca7a1128a494f8ca75d
Naif <knifffe@yandex.ru>
2022-12-12 09:33:28 +0300

    new

--- main.ts
+++ main.ts
@@ -2,28 +2,56 @@ input.onButtonPressed(Button.A, function () {
     a = 0
     led.enable(false)
 })
+function filter () {
+    c = c + (b - c) * 0.8
+    led.setBrightness(c)
+    c = c + (b - c) * 0.8
+    led.setBrightness(c)
+    c = c + (b - c) * 0.8
+    led.setBrightness(c)
+    c = c + (b - c) * 0.8
+    led.setBrightness(c)
+    c = c + (b - c) * 0.8
+    led.setBrightness(c)
+}
 input.onButtonPressed(Button.B, function () {
     a = 1
     led.enable(true)
 })
 let b = 0
+let c = 0
 let a = 0
 a = 0
+c = 255
+led.enable(false)
+led.plot(0, 0)
+led.plot(1, 0)
+led.plot(2, 0)
+led.plot(3, 0)
+led.plot(4, 0)
+led.plot(0, 1)
+led.plot(1, 1)
+led.plot(2, 1)
+led.plot(3, 1)
+led.plot(4, 1)
+led.plot(0, 2)
+led.plot(1, 2)
+led.plot(2, 2)
+led.plot(3, 2)
+led.plot(4, 2)
+led.plot(0, 3)
+led.plot(1, 3)
+led.plot(2, 3)
+led.plot(3, 3)
+led.plot(4, 3)
+led.plot(0, 4)
+led.plot(1, 4)
+led.plot(2, 4)
+led.plot(3, 4)
+led.plot(4, 4)
 basic.forever(function () {
     if (a == 1) {
         b = randint(0, 255)
-        if (b <= 50) {
-            b = 0
-        } else if (b <= 100) {
-            b = 50
-        } else if (b <= 150) {
-            b = 100
-        } else if (b <= 200) {
-            b = 150
-        } else {
-            b = 255
-        }
-        led.plotBrightness(randint(0, 4), randint(0, 4), b)
-        basic.pause(20)
+        filter()
     }
 })
